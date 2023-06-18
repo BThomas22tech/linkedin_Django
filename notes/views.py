3f30a07f004fabe3144f07e2bc3e23768a232d64
BThomas <bran_thom@yahoo.com>
2023-06-18 17:56:30 -0500

    validate form, edit, create, delete

--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,44 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from .models import Notes
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import NotesForm
+from django.http.response import HttpResponseRedirect
 
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
+    
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url ='/smart/notes'
+    form_class = NotesForm
+    
 class NotesCreateView(CreateView):
     model = Notes
-    fields = ['title','text']
+    form_class = NotesForm
     success_url ='/smart/notes'
     
-class NotesListView(ListView):
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+    
+class NotesListView(LoginRequiredMixin,ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
