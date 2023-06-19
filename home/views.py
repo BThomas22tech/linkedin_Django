53aeb7d9291e6327af28f4dcc415ce30089d1dfb
BThomas <bran_thom@yahoo.com>
2023-06-18 20:36:31 -0500

    course complete

--- home/views.py
+++ home/views.py
@@ -6,11 +6,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
-
-
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+    
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
+    
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
+    success_url = 'home/logout'
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
     
